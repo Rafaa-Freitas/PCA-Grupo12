5c8b00f03b28ce0087868f6205f725c3d75c264d
Rafael Freitas <69989761+Rafaa-Freitas@users.noreply.github.com>
2021-04-15 20:37:04 -0300

    Adicionando disparo de laser na nave do player

--- Clone Space Invaders/main.py
+++ Clone Space Invaders/main.py
@@ -36,20 +36,22 @@ class Laser:
         self.img = img
         self.mask = pygame.mask.from_surface(self.img)
 
-        def draw(self, window):
-            window.blit(self.img, (self.x, self.y))
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
 
-        def move(self, velocidade):
-            self.y += velocidade
+    def move(self, velocidade):
+        self.y += velocidade
 
-        def off_screen(self, height):
-            return self.y <= height and self.y >= 0
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0)
 
-        def collision(self, obj):
-            return collide(obj, self)
+    def collision(self, obj):
+        return collide(obj, self)
 
 # Classe padrão de naves
 class Nave:
+    COOLDOWN = 30
+
     def __init__(self, x, y, health = 100):
         self.x = x
         self.y = y
@@ -61,6 +63,32 @@ class Nave:
 
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
+        for laser in self.lasers:
+            laser.draw(window)
+
+    def move_lasers(self, velocidade, objs):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(velocidade)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= 10
+                self.lasers.remove(laser)
+
+
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
 
     def get_width(self):
         return self.ship_img.get_width()
@@ -75,6 +103,18 @@ class Player(Nave):
         self.laser_img = YELLOW_LASER
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
+
+    def move_lasers(self, velocidade, objs):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(velocidade)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            else:
+                for obj in objs:
+                    if laser.collision(obj):
+                        objs.remove(obj)
+                        self.lasers.remove(laser)
 
 class Enemy(Nave):
     COLOR_MAP = {
@@ -106,9 +146,11 @@ def main():
 
     enemies = []
     wave_lenght = 5
-    enemy_velocidade = 3
+    enemy_velocidade = 2
 
     player_velocidade = 10
+
+    laser_vel = 5
 
     player = Player(750, 800)
 
@@ -177,12 +219,16 @@ def main():
             player.y += player_velocidade
         if keys[pygame.K_ESCAPE]:
             run = False
+        if keys[pygame.K_SPACE]:
+            player.shoot()
 
         for enemy in enemies[:]:
             enemy.move(enemy_velocidade)
-
+            enemy.move_lasers(laser_vel, player)
             if enemy.y + enemy.get_height() > HEIGHT:
                 vidas -= 1
                 enemies.remove(enemy)
+
+        player.move_lasers(-laser_vel, enemies)
 
 main()
